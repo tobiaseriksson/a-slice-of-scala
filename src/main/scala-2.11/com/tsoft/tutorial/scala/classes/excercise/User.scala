8ff3e671b334e7aacbc6de760883509d1b943f1a
Tobias Eriksson <Tobias.Eriksson@qvantel.com>
2016-05-19 21:45:32 +0200

    Added excercise solution

---
+++ src/main/scala-2.11/com/tsoft/tutorial/scala/classes/excercise/User.scala
@@ -0,0 +1,49 @@
+package com.tsoft.tutorial.scala.classes.excercise
+
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+
+case class User(name:String,age:Int,gender:String) {
+
+}
+
+object Utils {
+  val rand = Random
+
+  def generateLots(howMany:Int) = {
+    (1 to howMany).map( _ => {
+      User( randomName, randomAge, randomGender )
+    })
+  }
+
+  def randomName = {
+    val names = Seq( "Tobias", "Marcus", "Ingvar", "Anna", "Lisa", "Elisabeth", "Karl", "Anders", "Henrik", "Petter", "Michael", "Cecilia", "Maria", "Christian", "Bob")
+    names( rand.nextInt(names.size) )
+  }
+
+  def randomAge = rand.nextInt(100)
+
+  def randomGender = {
+    val genders = Seq( "Male", "Female" )
+    genders( rand.nextInt(2) )
+  }
+
+  def main(args: Array[String]) {
+
+    val users = generateLots(100)
+    users.foreach( println )
+
+    val topFemaleNames =
+      users.filter( u => u.gender=="Female" )   // Pick out the "Female"s only => Seq[User]
+      .groupBy( u => u.name )  // group-by the users name => map of (String,Seq[User])
+      .mapValues( _.size )  // iterate over the values of the map and calc the size of each Seq[User] => Map[String,Int]
+      .toList // Convert it into a list of tuple2's List[ (String,Int) ]
+      .sortBy( _._2 )(Ordering[Int].reverse) // sort the list by the second tuple element, i.e. the size, and reverse the sorting order
+      .take(10) // take the 10 first elements in the list => List[(String,Int)]
+
+    println("Top-10 Female names in this list")
+    topFemaleNames.foreach(println)
+
+  }
+
+}
--- src/main/scala-2.11/com/tsoft/tutorial/scala/classes/step3/individual.scala
+++ src/main/scala-2.11/com/tsoft/tutorial/scala/classes/step3/individual.scala
@@ -17,22 +17,21 @@ class Individual(val firstName:String,val lastName:String,val favoriteColor:Stri
 case class IndividualCC(firstName:String, lastName:String,favoriteColor:String="Red") {
 
   def getName() = firstName + " " + lastName
-  override def toString() : String = firstName+" "+lastName+" with favourite color "+favoriteColor
+  override def toString : String = firstName+" "+lastName+" with favourite color "+favoriteColor
 }
 
 object Demo {
   def main(args: Array[String]) {
     val ind = new Individual("Tobias","Eriksson","Blue")
     /* We can not do this anymore
-    ind.setFirstName("Ingvar")
     ind.firstName="Marcus"
     */
     println("Name:"+ind)
 
     val ind2 = IndividualCC("Marcus","Holgersson","Yellow")  // NOTE! that 'new' is not required for Case Classes !!!
-    /* we can not do this anymore, the case class is by default immutable
-    ind2.firstName="Ingvar"
-    BUT we can do this
+    /* we can not do this anymore, the case class is by default immutable */
+    // ind2.firstName="Ingvar"
+    /* BUT we can do this
     */
     val ind3 = ind2.copy( firstName = "Ingvar", lastName = "Abrahamsson" ) // with the copy we preserv the attributes not mentioned in the copy, this is convenient
     println("Orig Name:"+ind2)
